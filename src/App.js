029350dc65e316b3851fb044eca3b0e86eb66748
joya <joyanyanwu14@gmail.com>
2020-07-30 22:11:24 +0100

    completed basic app - need to change styling and filtering options

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 import Job from './Job'
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
+  function handleParamChange(e){
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams =>{
+      return {...prevParams, [param]: value}
+    })
+  }
+
   return (
     <Container className="my-4">
       <h1 className="mb-4"> GitHub Jobs</h1>
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true}/>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
       {loading && <h1> Loading.. </h1>}
       {error && <h1> Error. Please refresh the page. </h1>}
   
       {jobs.map(job => {
         return <Job key={job.id} job ={job}> </Job>
       })}
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true}/>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
     </Container>
 
   )
