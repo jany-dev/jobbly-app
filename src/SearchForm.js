16f1fdcfa7a261ecf5466f181f487eb50a09666f
joya <joyanyanwu14@gmail.com>
2020-09-16 21:11:45 +0100

    started adding react multi select componenet

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Form, Col, Badge, Collapse } from 'react-bootstrap';
 import Collapsible from 'react-collapsible';
 import Grid from '@material-ui/core/Grid';
+import Select from 'react-select';
+
+
+const jobType = [
+    {name: "Full-Time"},
+    {name: "Part-Time"},
+    {name: "Temporary"},
+    {name: "Permanent"},
+    {name: "Contract"}
+]
+
 
 export default function SearchForm({ params, onParamChange }) {
     return (
@@ -17,12 +28,25 @@ export default function SearchForm({ params, onParamChange }) {
                 <Form.Row >
                     <Col md className="text-center">
                         <Form.Label className="pt-4"> Location</Form.Label>
-                        <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" id="location" classNameclassName="justify-content-center"/>
+                        <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" id="location" classNameclassName="justify-content-center" />
                     </Col>
                 </Form.Row>
+
+
                 <Form.Row className="d-flex justify-content-center">
                     <Col>
                         <Form.Label className="pt-4"> Job Type</Form.Label>
+                        <Select
+                            getOptionLabel={(option) => {
+                                return option.name
+                            }}
+                            defaultValue={[]}
+                            isMulti
+                            name="job-type"
+                            options={jobType}
+                            className="basic-multi-select"
+                            classNamePrefix="select"
+                        />
                         <Grid container spacing={1} className=" justify-content-center" style={{ flexDirection: 'row' }}>
                             <Grid item md={4}>
                                 <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" id="full-time" label="Full-Time" type="checkbox" className="mb-2 text-muted" />
@@ -49,11 +73,12 @@ export default function SearchForm({ params, onParamChange }) {
 
                     <Col>
                         <Form.Label className="pt-4">Salary Range</Form.Label>
-                        <Grid container spacing={1}className=" justify-content-center" style={{ flexDirection: 'row' }}>
+                        <Grid container spacing={1} className=" justify-content-center" style={{ flexDirection: 'row' }}>
                             <Grid item md={4}>
-                                <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" id="full-time" label="Full-Time" type="checkbox" className="mb-2 text-muted" />
-                                <Form.Check onChange={onParamChange} value={params.javascript} name="part_time" id="part-time" label="Part-Time" type="checkbox" className="mb-2 text-muted" />
-                                <Form.Check onChange={onParamChange} value={params.temporary} name="temporary" id="javascript" label="Temporary" type="checkbox" className="mb-2 text-muted" />
+                                <Form.Check onChange={onParamChange} value={params["20,000"]} name="20,000" id="low" label="< 20,000" type="checkbox" className="mb-2 text-muted" />
+                                <Form.Check onChange={onParamChange} value={params["20,000 - 30,000"]} name="20,000 - 30,000" id="part-time" label="20,000 - 30,000" type="checkbox" className="mb-2 text-muted" />
+                                <Form.Check onChange={onParamChange} value={params["31,000 - 40,000"]} name="31,000 - 40,000" id="javascript" label="31,000 - 40,000" type="checkbox" className="mb-2 text-muted" />
+                                <Form.Check onChange={onParamChange} value={params["> 40,000"]} name="> 40,000" id="javascript" label="> 40,000" type="checkbox" className="mb-2 text-muted" />
 
                             </Grid>
                         </Grid>
